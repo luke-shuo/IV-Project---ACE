1cf76d5bc537fbedfb2a597d95887554c61944dc
Zhang Shuo <2819601S@student.gla.ac.uk>
2023-03-07 11:44:23 +0000

    EU map

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import streamlit as st
 data = pd.read_csv("/Users/lukeshuo/Documents/GitHub/Altair/europe_covid.csv")
 chart = alt.Chart(data)
 
-line = chart.mark_line().encode(
-  alt.Y('Total Deaths', scale=alt.Scale(type='log')),
-  alt.X('Country/Other')).interactive()
+point = chart.mark_point().encode(
+  y='Country/Other',
+  x='Total Cases',
+  color='Country/Other',
+  tooltip=['Country/Other', 'Total Cases']
+)
 
-bar = chart.mark_bar().encode(alt.X('Country/Other'), alt.Y('Total Deaths'))
-
-c = line & bar
 # c.save('chart.html')
 st.title('IV Project')
 
-st.altair_chart(c, use_container_width=True)
+st.altair_chart(point, use_container_width=True)
