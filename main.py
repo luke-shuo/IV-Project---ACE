37102b6d79c6fd9c9ec4d9f9c5f4c269a446f5c2
Zhang Shuo <2819601S@student.gla.ac.uk>
2023-03-13 16:16:38 +0000

    EU map with Covid Data

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import pandas as pd
 import altair as alt
 import streamlit as st
+import geopandas as gpd
 
 data = pd.read_csv("/Users/lukeshuo/Documents/GitHub/Altair/europe_covid.csv")
-chart = alt.Chart(data)
+data = pd.read_csv("/Users/lukeshuo/Documents/GitHub/Altair/europe_covid.csv")
+data = data[data.Country != 'Channel Islands']
+data = data[data.Country != 'Gibraltar']
+data = data[data.Country != 'Isle of Man']
+data.replace(['UK'], ['United Kingdom'], inplace=True)
+data.replace(['Czechia'], ['Czech Republic'], inplace=True)
+data.replace(['Faeroe Islands'], ['Faroe Islands'], inplace=True)
+data.replace(['Moldova'], ['Republic of Moldova'], inplace=True)
+data.replace(['North Macedonia'], ['The former Yugoslav Republic of Macedonia'], inplace=True)
 
-point = chart.mark_point().encode(
-  y='Country/Other',
-  x='Total Cases',
-  color='Country/Other',
-  tooltip=['Country/Other', 'Total Cases']
+eu_data = gpd.read_file('/Users/lukeshuo/Documents/GitHub/Altair/materials/europe-economy-visualization-master/data/europe.geojson')
+
+font = 'Ubuntu Mono'
+red = '#d53e4f'
+blue = '#3288bd'
+gray = '#5D646F'
+
+chart = alt.Chart(eu_data, width=1040, height=640).mark_geoshape().encode(
+    color=alt.condition('datum["Total Deaths"] >= -1', alt.Color('Total Deaths:Q'), alt.value('Total Deaths:Q')),
+    tooltip = ['NAME', 'Total Deaths:Q']
+).project(
+    'naturalEarth1'
+).transform_lookup(
+    lookup='NAME',
+    from_=alt.LookupData(data, 'Country', ['Total Deaths'])
 )
 
-# c.save('chart.html')
+
+chart.save('chart.html')
 st.title('IV Project')
 
-st.altair_chart(point, use_container_width=True)
+st.altair_chart(chart)
